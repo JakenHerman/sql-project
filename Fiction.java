e5f08be6c5797c8729d952cfa0aef17b55a89d41
Addison Gauss <Addison Gauss>
2016-02-02 16:59:50 -0600

    trying to upload library project

---
+++ Fiction.java
@@ -0,0 +1,42 @@
+
+public class Fiction extends Book
+{
+
+	private char type;
+
+    public Fiction()
+    {
+    	super();            // call super class constructor
+		type = 'F';
+
+    }
+
+    public Fiction(String title,
+                 String author,char type)
+   {
+   	   
+   	   super(title, author);                 //call Book's overloaded constructor
+   	   setType(type);
+
+   }
+
+   public char getType()
+   {
+   	 return type;
+   }
+
+
+   public void setType(char newType)
+   {
+   		type = newType;
+
+   }
+   public String toString()
+   {
+		String out = super.toString() + " \nGenre = Fiction";
+		return out;
+   }
+
+
+
+}
