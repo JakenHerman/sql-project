6c8b183a87cdbbfababfc70aa37411026fc432ec
Addison Gauss <addisongauss93@yahoo.com>
2016-03-17 10:59:30 -0500

    Added files via upload
    If you have a database, try to hook yours up to this code

--- Login.java
+++ Login.java
@@ -17,35 +17,32 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-
+import java.sql.ResultSet;
+import java.sql.Connection;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.*;
+import java.sql.ResultSet;
 
-
-// Library program. Admin can add or delete books from the Library. 
-// User's can login and checkin/checkout books to add to their list.
-// On exiting, program should save all additions/deletions of books to a text file.
-// When used again, program will read saved info from text file and update GUI
-
-@SuppressWarnings({ "serial", "unused" })
-public class Login extends JFrame
-{
-	
-	//GUI 
+public class Login extends javax.swing.JFrame {
+ Connection con = null;
+ ResultSet rst = null;
+ PreparedStatement pts = null;
  private Container screen;
  private JLabel idLabel, passwordLabel, message;
  private JPasswordField password;
  private JTextField id;
-
-
- // Constructor
- public Login( )
- {
-  super( "Login" );
+    
+    public Login() {
+        super( "Login" );
+       
+       
+        
   screen = getContentPane( );
   screen.setLayout( new FlowLayout( ) );
  
@@ -77,131 +74,93 @@ public class Login extends JFrame
   
   
  }
- 
- public class Choices implements ActionListener         // What to do when button is pressed
+    
+public class Choices implements ActionListener         // What to do when button is pressed
  {
  public void actionPerformed( ActionEvent eventButton )
  {
+    
+     boolean found = false;
+    
 	// If login is pressed and username/password are NOT EMPTY
  	if(eventButton.getActionCommand().equals("Login"))
  	{
- 		
-    boolean found = false; // boolean value, defaulted to false, true whenever correct login information is entered
-	String userID = " ";      // Place holders for the file to read and enter information                   
-    String userPassword = " ";                       
-    String name = " "; 
-	String ID = id.getText();
-	String PASS = new String(password.getPassword());
-  	ArrayList<Object>myList = new ArrayList<Object>();  //Create Array List of objects to hold user's books
-  
-  	   
-  	if(ID.equals("admin") && ( PASS.equals("password")) && ((!ID.equalsIgnoreCase("") && !PASS.equals(""))))
-   	{
-   		
-        found = true;
-   		GUI adminGUI = new GUI();
-
-   		
-   	}
-   	else if (!ID.equals("admin") && ( !PASS.equals("password")) && ((!ID.equalsIgnoreCase("") || !PASS.equals("")))){  // else admin info not entered, so we will see if we have the user info already saved in text file.                                
-		try
-	   {
-	   	 Object tbook;        // Object variable to hold a book
-	   	 String author1,title1;
-
-	   	 String userid = id.getText();
-	   	 String userpass = new String(password.getPassword());
-
-	     FileReader fr = new FileReader( "user.txt");   // read from user.txt
-	     BufferedReader br = new BufferedReader( fr );
-	     myList.clear();
-
-	     
-	     String stringRead = br.readLine( );      //store read values into String variable
-	  
-	     while( stringRead != null || !found) // end of the file?
-	     {
-			// read lines
-	    	 
-	    	 StringTokenizer st = new StringTokenizer( stringRead, "," );  // Every time a "," is encountered in the text file it saves everything read since the last comma
-		     userID = st.nextToken( );      // User Id is listed first                              
-		     userPassword = st.nextToken(); // Password is after userID                             
-		     name = st.nextToken();         // user's actual name                               
-		     int noBooks = Integer.parseInt(st.nextToken()); // number of books in user list
-
-
-
-		       	// if the user entered a name and password that equals an already saved username and password
-		   	if (userid.equals(userID) && (userpass.equals(userPassword)))        
-		   	{                                                                    
-		  
-		   		found = true;
-
-			   	for(int ct = 0; ct < noBooks; ct++)      //Loop through the number of books previously saved
-			   	{
-				   		String title = st.nextToken( );  
-		       			String author = st.nextToken();
-		       			char type = st.nextToken().charAt(0); //Save the genre
-
-                      
-				    if (type == 'F') // if genre = fiction
-				    {
-				    	Fiction FicBook = new Fiction(title,author,'F');  // create a Fiction book with the read value from text file
-						myList.add(FicBook);      // add to array list to hold current user's books                 
-						myList.trimToSize();      
-				    }
-					else if (type == 'N') // if genre = nonfiction
-				    {
-
-						NonFiction nonFicBook = new NonFiction(title,author,'N');
-						myList.add(nonFicBook);
-						myList.trimToSize();
-				    }
-
-
-
-				}
-		  }
-
-
-			stringRead = br.readLine( );
-	     }
-
-
-			User tempUser = new User(userID,userPassword,name,myList);        //Create user object to hold the read user's data
-			userGUI tempGUI = new userGUI(tempUser);                         // Create GUI for the user
-			br.close( );
-	   }
-
-   	
-	 	catch( FileNotFoundException fnfe )
-	   {
-	     System.out.println( "Unable to find user.txt" );
-	   }
-
-	   catch( IOException ioe )
-	   {
-	     ioe.printStackTrace( );
-	   }
-
-		}
-
-
-
-if (!found)
-{
-	JOptionPane.showMessageDialog(null,
-    		"Title or author is empty or not valid");
-}
-}
- 
+            
+            try {
+      Class.forName("sun.jdbc.odbc.JdbcOdbcDriver"); // may not be needed since JDBC4
+      Connection con=DriverManager.getConnection("jdbc:odbc:MessageStore","sa","12345"); 
+      pts=con.prepareStatement("select * from UserList where username=? and password=?");
+      pts.setString(1,id.getText());
+      pts.setString(2, password.getText());
+      ResultSet rs=pts.executeQuery();
+      if( rs.next() ){
+       found = true;
+      }else{
+       found = false;
+      }
+    
+    
+    
+    }catch(Exception e){}
+    
+        }
  }
- } 
-	 public static void main( String [] args )
-	 {
-		 
-	  Login login = new Login( );
-	  login.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	  
-	 }
 }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    
+    }
+}
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
